531fcb2508ca633a0d379564285927c774f9aaf0
tmuhader <58604675+tmuhader@users.noreply.github.com>
2020-05-27 23:29:17 +0400

    added jenkinsfile

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -1,4 +1,4 @@
-package com.tmuhader.Calculator;
+package com.leszko.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
